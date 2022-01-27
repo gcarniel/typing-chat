49deaf93e4137c28dcea75d1664d45a2ba9538c4
gcarniel <gcarniel@outlook.com>
2022-01-26 23:04:28 -0300

    finalizando aula 3

---
+++ pages/chat.jsx
@@ -0,0 +1,234 @@
+import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import React, { useState } from "react";
+import { FiSend, FiTrash2 } from "react-icons/fi";
+import appConfig from "../config.json";
+
+export default function ChatPage() {
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  function handleNewMessage(newMessage) {
+    const message = {
+      id: messageList.length + 1,
+      text: newMessage,
+      from: "alguem",
+    };
+    setMessageList([message, ...messageList]);
+    setMessage("");
+  }
+
+  return (
+    <Box
+      styleSheet={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+          borderRadius: "5px",
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: "100%",
+          maxWidth: "95%",
+          maxHeight: "95vh",
+          padding: "32px",
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: "relative",
+            display: "flex",
+            flex: 1,
+            justifyContent: "flex-end",
+            height: "80%",
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: "column",
+            borderRadius: "5px",
+            padding: "16px",
+          }}
+        >
+          <MessageList
+            messageList={messageList}
+            setMessageList={setMessageList}
+          />
+
+          <Box
+            as="form"
+            styleSheet={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  if (message) {
+                    handleNewMessage(message);
+                  }
+                }
+              }}
+              placeholder="Insira sua messagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: "100%",
+                border: "0",
+                resize: "none",
+                borderRadius: "5px",
+                padding: "6px 8px",
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                // marginRight: "12px",
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+            />
+            <Box
+              styleSheet={{
+                position: "relative",
+                display: "flex",
+                flex: 1,
+                height: "80%",
+                backgroundColor: appConfig.theme.colors.neutrals[600],
+                padding: "10px",
+              }}
+            >
+              <FiSend
+                style={{
+                  cursor: `${message.length > 0 ? "pointer" : "not-allowed"}`,
+                }}
+                onClick={() => {
+                  if (message) {
+                    handleNewMessage(message);
+                  }
+                }}
+              />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <Box
+        styleSheet={{
+          width: "100%",
+          marginBottom: "16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button
+          variant="tertiary"
+          colorVariant="neutral"
+          label="Logout"
+          href="/"
+        />
+      </Box>
+    </>
+  );
+}
+
+function MessageList(props) {
+  function deleteMessage(messageId) {
+    const newMessages = props.messageList.filter(
+      (message) => message.id !== messageId
+    );
+    props.setMessageList(newMessages);
+  }
+
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: "auto",
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: "16px",
+      }}
+    >
+      {props.messageList.map((message) => {
+        return (
+          <Text
+            key={message.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: "5px",
+              padding: "6px",
+              marginBottom: "12px",
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              },
+            }}
+          >
+            <Box
+              styleSheet={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                styleSheet={{
+                  marginBottom: "8px",
+                }}
+              >
+                <Image
+                  styleSheet={{
+                    width: "20px",
+                    height: "20px",
+                    borderRadius: "50%",
+                    display: "inline-block",
+                    marginRight: "8px",
+                  }}
+                  src={`https://github.com/gcarniel.png`}
+                />
+                <Text tag="strong">{message.from}</Text>
+                <Text
+                  styleSheet={{
+                    fontSize: "10px",
+                    marginLeft: "8px",
+                    color: appConfig.theme.colors.neutrals[300],
+                  }}
+                  tag="span"
+                >
+                  {new Date().toLocaleDateString()}
+                </Text>
+              </Box>
+              <Text tag="span" styleSheet={{ opacity: "50" }}>
+                <FiTrash2
+                  size={10}
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    deleteMessage(message.id);
+                  }}
+                />
+              </Text>
+            </Box>
+            {message.text}
+          </Text>
+        );
+      })}
+    </Box>
+  );
+}
